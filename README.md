24904c910f98d66e634033f9042a961b8cf87744
Francisco González <fgb_03@hotmail.com>
2022-05-18 19:43:12 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# pgonbo_calculadora
+
+- Es una librería que suma, resta y multiplica, pero se desarrollo solo para aprender a subir paquetes a npm, 
+  por lo que no se recomienda su uso, ni instalación
+  
+## Una vez dada la advertencia es responsabilidad de cada uno el usarla o no.
---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * suma dos numeros
+ * @param a
+ * @param b
+ * @returns {*}
+ */
+export const suma=(a,b)=>a+b;
+/**
+ * resta de dos numeros
+ * @param a
+ * @param b
+ * @returns {number}
+ */
+export const resta=(a,b)=>a-b;
+/**
+ * Multiplicación de dos números
+ * @param a
+ * @param b
+ * @returns {number}
+ */
+export const multiplicacion=(a,b)=>a*b;
